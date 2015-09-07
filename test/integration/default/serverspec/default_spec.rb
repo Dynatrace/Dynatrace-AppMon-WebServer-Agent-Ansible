0024a01026e660e96a2fe1415f96cdc72c594c17
Martin Etmajer <m.etmajer@gmail.com>
2015-09-07 18:24:08 +0200

    Improve test suite.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -20,8 +20,9 @@ describe file('/opt/dynatrace/agent') do
 end
 
 describe file ('/opt/dynatrace/agent/conf/dtwsagent.ini') do
-  its(:content) { should match /Name .+/ }
-  its(:content) { should match /Server .+/ }
+  its(:content) { should match /^Name dtwsagent$/ }
+  its(:content) { should match /^Server localhost:9998$/ }
+  its(:content) { should match /^Loglevel info$/ }
 end
 
 describe file ('/etc/init.d/dynaTraceWebServerAgent') do
@@ -29,20 +30,24 @@ describe file ('/etc/init.d/dynaTraceWebServerAgent') do
   it { should be_grouped_into 'root' }
 
   if os[:family] == 'debian' || os[:family] == 'ubuntu'
-    its(:content) { should match /^\#Default-Start: 2 3 4 5$/ }
-    its(:content) { should match /^\#Default-Stop: 0 1 6$/ }
+    its(:content) { should match /^\# Default-Start: 2 3 4 5$/ }
+    its(:content) { should match /^\# Default-Stop: 0 1 6$/ }
   elsif os[:family] == 'redhat'
-    its(:content) { should match /^\#Default-Start: 3 5$/ }
-    its(:content) { should match /^\#Default-Stop: 0 1 2 6$/ }
+    its(:content) { should match /^\# Default-Start: 3 5$/ }
+    its(:content) { should match /^\# Default-Stop: 0 1 2 6$/ }
   end
 
   its(:content) { should match /^DT_HOME=\/opt\/dynatrace$/ }
   its(:content) { should match /^.*su - dynatrace -c.*$/ }
 end
 
+describe process('dtwsagent') do
+  it { should be_running }
+  its(:user) { should eq 'dynatrace' }
+end
+
 describe service('dynaTraceWebServerAgent') do
   it { should be_enabled }
-  it { should be_running }
 
   if os[:family] == 'debian' || os[:family] == 'ubuntu'
       it { should be_enabled.with_level(3) }
